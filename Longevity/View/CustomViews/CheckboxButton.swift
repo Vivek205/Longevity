7bcc93ad7d67e2fee57dde7b5def071ae2a8360e
Vivek205 <ichbinvivek@gmail.com>
2020-07-27 21:44:56 +0530

    get questions from mockAPI

---
+++ Longevity/View/CustomViews/CheckboxButton.swift
@@ -0,0 +1,30 @@
+//
+//  CheckboxButton.swift
+//  Longevity
+//
+//  Created by vivek on 13/07/20.
+//  Copyright © 2020 vivek. All rights reserved.
+//
+
+import UIKit
+
+class CheckboxButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        styleButton()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        styleButton()
+    }
+
+    func styleButton() {
+        layer.masksToBounds = true
+        setImage(#imageLiteral(resourceName: "icon: Checkbox-unselected"), for: .normal)
+        setImage(#imageLiteral(resourceName: "icon: Checkbox-selected"), for: .selected)
+        layer.cornerRadius = frame.width / 2
+        setTitleColor(UIColor.white, for: .selected)
+    }
+
+}
