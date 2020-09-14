c74e910a4f36c888d6f82a9e79c29f7b67197db0
Jagan Kumar <mudila.jagan@shrenitexpert.online>
2020-09-15 00:40:06 +0400

    Updated Biometric and Devices

--- Longevity/Controller/SetupProfile/SetupProfileBioDataVC.swift
+++ Longevity/Controller/SetupProfile/SetupProfileBioDataVC.swift
@@ -21,14 +21,14 @@ class SetupProfileBioDataVC: UIViewController {
     @IBOutlet weak var footerView: UIView!
     
     let healthKitUtil: HealthKitUtil = HealthKitUtil.shared
-
+    
     var toolBar = UIToolbar()
     var picker  = UIPickerView()
     var agePicker = UIDatePicker()
     var pickerData = [String]()
     var selectedPickerValue:String = ""
     var selectedAgePickerValue:Date = Date()
-
+    
     var isFromSettings: Bool = false
     
     override func viewDidLoad() {
@@ -38,28 +38,35 @@ class SetupProfileBioDataVC: UIViewController {
         collectionView.dataSource = self
         continueButton.isEnabled = false
         createPickersAndToolbar()
-
+        
         checkIfHealthKitSyncedAlready()
         
         if self.isFromSettings {
             self.viewProgressBar.isHidden = true
-            let leftbutton = UIBarButtonItem(image: UIImage(named: "icon: arrow")?.withHorizontallyFlippedOrientation(), style: .plain, target: self, action: #selector(closeView))
+            let leftbutton = UIBarButtonItem(title:"Cancel", style: .plain, target: self, action: #selector(closeView))
             leftbutton.tintColor = .themeColor
             let rightButton = UIBarButtonItem(title:"Done", style: .plain, target: self, action: #selector(doneUpdate))
-              rightButton.tintColor = .themeColor
+            rightButton.tintColor = .themeColor
             self.viewNavigationItem.leftBarButtonItem = leftbutton
             self.viewNavigationItem.rightBarButtonItem = rightButton
             self.footerView.isHidden = true
         }
+        
+        guard let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else {
+            return
+        }
+        layout.minimumLineSpacing = 0
+        layout.scrollDirection = .vertical
+        layout.invalidateLayout()
     }
-
+    
     func checkIfHealthKitSyncedAlready() {
         if healthKitUtil.isHealthkitSynced {
             self.readHealthData()
             continueButton.isEnabled = true
         }
     }
-
+    
     func createPickersAndToolbar() {
         // Picker
         picker.delegate = self
@@ -69,7 +76,7 @@ class SetupProfileBioDataVC: UIViewController {
         picker.backgroundColor = UIColor.white
         picker.frame = CGRect.init(x: 0.0, y: UIScreen.main.bounds.size.height - 300, width: UIScreen.main.bounds.size.width, height: 300)
         picker.setValue(UIColor.sectionHeaderColor, forKey: "textColor")
-
+        
         // DatePicker
         agePicker.autoresizingMask = .flexibleWidth
         agePicker.contentMode = .center
@@ -80,26 +87,26 @@ class SetupProfileBioDataVC: UIViewController {
         agePicker.addTarget(self, action: #selector(onAgeChanged(sender:)), for: .valueChanged)
         agePicker.maximumDate = Date()
         agePicker.setValue(UIColor.sectionHeaderColor, forKey: "textColor")
-
+        
         // Toolbar
         toolBar = UIToolbar.init(frame: CGRect.init(x: 0.0, y: UIScreen.main.bounds.size.height - 300, width: UIScreen.main.bounds.size.width, height: 50))
         toolBar.barStyle = .blackTranslucent
         toolBar.items = [UIBarButtonItem.init(title: "Done", style: .done, target: self, action: #selector(onDoneButtonTapped))]
-
+        
     }
-
+    
     func actualValue(selectedOption: String) -> Double {
         let indexOfSpace = selectedOption.firstIndex(of: " ")!
         let valueString: String = String(selectedOption[..<indexOfSpace])
         return Double(valueString)!
     }
-
+    
     // MARK: Actions
     @IBAction func handleContinue(_ sender: Any) {
         updateHealthProfile()
         performSegue(withIdentifier: "SetupProfileBioDataToNotification", sender: self)
     }
-
+    
     @IBAction func handleMetricTogglePress(_ sender: Any) {
         healthKitUtil.toggleSelectedUnit()
         guard healthKitUtil.isHealthkitSynced else {return}
@@ -120,14 +127,14 @@ class SetupProfileBioDataVC: UIViewController {
             if let weight = healthKitUtil.getWeightStringInImperial() {
                 setupProfileOptionList[6]?.buttonText = "\(weight) \(healthKitUtil.selectedUnit.weight)"
             }
-
+            
             break
         }
-
+        
         self.collectionView.reloadData()
         return
     }
-
+    
     func authorizeHealthKitInApp() {
         healthKitUtil.authorize
             { (success, error) in
@@ -153,41 +160,41 @@ class SetupProfileBioDataVC: UIViewController {
                 setupProfileOptionList[3]?.buttonText = biologicalSex.string
             }
         }
-
-            healthKitUtil.readHeightData {
-                (height, error) in
-                guard let heightSample = height as? HKQuantitySample else {return}
-                let heightString = self.healthKitUtil.getHeightString(from: heightSample)
-                DispatchQueue.main.async {
-                    setupProfileOptionList[5]?.buttonText = heightString ?? ""
-                    setupProfileOptionList[5]?.isSynced = true
-                    self.continueButton.isEnabled = true
-                    self.collectionView.reloadData()
-                }
+        
+        healthKitUtil.readHeightData {
+            (height, error) in
+            guard let heightSample = height as? HKQuantitySample else {return}
+            let heightString = self.healthKitUtil.getHeightString(from: heightSample)
+            DispatchQueue.main.async {
+                setupProfileOptionList[5]?.buttonText = heightString ?? ""
+                setupProfileOptionList[5]?.isSynced = true
+                self.continueButton.isEnabled = true
+                self.collectionView.reloadData()
             }
-
-            healthKitUtil.readWeightData {
-                (weight, error) in
-                if error != nil {
-                    return
-                }
-                guard let weightSample = weight as? HKQuantitySample else {return}
-                let weightString = self.healthKitUtil.getWeightString(from: weightSample)
-                DispatchQueue.main.async {
-                    setupProfileOptionList[6]?.buttonText = weightString ?? ""
-                    setupProfileOptionList[6]?.isSynced = true
-                    self.continueButton.isEnabled = true
-                    self.collectionView.reloadData()
-                }
+        }
+        
+        healthKitUtil.readWeightData {
+            (weight, error) in
+            if error != nil {
+                return
             }
-            self.continueButton.isEnabled = true
-            self.collectionView.reloadData()
+            guard let weightSample = weight as? HKQuantitySample else {return}
+            let weightString = self.healthKitUtil.getWeightString(from: weightSample)
+            DispatchQueue.main.async {
+                setupProfileOptionList[6]?.buttonText = weightString ?? ""
+                setupProfileOptionList[6]?.isSynced = true
+                self.continueButton.isEnabled = true
+                self.collectionView.reloadData()
+            }
+        }
+        self.continueButton.isEnabled = true
+        self.collectionView.reloadData()
     }
     
     @objc func closeView() {
         self.dismiss(animated: true, completion: nil)
     }
-
+    
     @objc func doneUpdate() {
         updateHealthProfile()
         self.dismiss(animated: true, completion: nil)
@@ -201,35 +208,35 @@ extension SetupProfileBioDataVC: SetupProfileBioOptionCellDelegate {
         static let agePicker = "age"
         static let genderPicker = "gender"
     }
-
+    
     func showPicker() {
         self.view.addSubview(picker)
         self.view.addSubview(toolBar)
     }
-
+    
     func removePickers() {
         toolBar.removeFromSuperview()
         picker.removeFromSuperview()
         agePicker.removeFromSuperview()
     }
-
+    
     @objc func onDoneButtonTapped() {
         removePickers()
         updateUserDefaultsOnPickerDone()
     }
-
+    
     func updateUserDefaultsOnPickerDone(selectedRow: Int = 0) {
-
+        
         switch picker.accessibilityLabel {
         case PickerLabel.agePicker:
             let dateFormatter: DateFormatter = DateFormatter()
             dateFormatter.dateFormat = "dd-MM-yyyy"
             let selectedDate: String = dateFormatter.string(from: selectedAgePickerValue)
-
+            
             let calendar = Calendar.current
             let ageComponents = calendar.dateComponents([.year], from: selectedAgePickerValue, to: Date())
             let currentAge = ageComponents.year!
-
+            
             setupProfileOptionList[4]?.buttonText = "\(currentAge) years"
             setupProfileOptionList[4]?.isSynced = true
             AppSyncManager.instance.healthProfile.value?.birthday = selectedDate
@@ -245,16 +252,16 @@ extension SetupProfileBioDataVC: SetupProfileBioOptionCellDelegate {
                 splitValue = healthKitUtil.getCentimeter(fromFeet: splitValue)
             }
             AppSyncManager.instance.healthProfile.value?.height = splitValue
-
+            
         case PickerLabel.weightPicker:
             setupProfileOptionList[6]?.buttonText = "\(selectedPickerValue)"
             setupProfileOptionList[6]?.isSynced = true
-             var splitValue = selectedPickerValue.components(separatedBy: " ")[0]
+            var splitValue = selectedPickerValue.components(separatedBy: " ")[0]
             if healthKitUtil.selectedUnit == .imperial {
                 splitValue = healthKitUtil.getKilo(fromPounds: splitValue)
             }
             AppSyncManager.instance.healthProfile.value?.weight = splitValue
-
+            
         default:
             print(picker.accessibilityLabel)
         }
@@ -262,11 +269,11 @@ extension SetupProfileBioDataVC: SetupProfileBioOptionCellDelegate {
             self.collectionView.reloadData()
         }
     }
-
+    
     @objc func onAgeChanged(sender: UIDatePicker) {
         selectedAgePickerValue = sender.date
     }
-
+    
     func showGenderPicker() {
         picker.selectRow(0, inComponent: 0, animated: true)
         pickerData = ["male", "female","other"]
@@ -275,7 +282,7 @@ extension SetupProfileBioDataVC: SetupProfileBioOptionCellDelegate {
         picker.reloadAllComponents()
         showPicker()
     }
-
+    
     func showHeightPicker() {
         pickerData = healthKitUtil.getHeightPickerOptions()
         picker.selectRow(30, inComponent: 0, animated: true)
@@ -284,7 +291,7 @@ extension SetupProfileBioDataVC: SetupProfileBioOptionCellDelegate {
         picker.reloadAllComponents()
         showPicker()
     }
-
+    
     func showWeightPicker() {
         pickerData = healthKitUtil.getWeightPickerOptions()
         picker.selectRow(30, inComponent: 0, animated: true)
@@ -293,14 +300,14 @@ extension SetupProfileBioDataVC: SetupProfileBioOptionCellDelegate {
         picker.reloadAllComponents()
         showPicker()
     }
-
+    
     func showAgePicker() {
         removePickers()
         picker.accessibilityLabel = PickerLabel.agePicker
         self.view.addSubview(agePicker)
         self.view.addSubview(toolBar)
     }
-
+    
     func button(wasPressedOnCell cell: SetupProfileBioOptionCell) {
         switch cell.label.text {
         case "Sync Apple Health Profile":
@@ -324,18 +331,18 @@ extension SetupProfileBioDataVC: UIPickerViewDelegate, UIPickerViewDataSource {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-
+    
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-
+        
         return pickerData.count
     }
-
+    
     func pickerView(_ pickerView: UIPickerView, titleForRow pickerRow: Int, forComponent component: Int) -> String? {
         //        print("\(pickerData[row])")
-
+        
         return String(pickerData[pickerRow])
     }
-
+    
     func pickerView(_ pickerView: UIPickerView, didSelectRow pickerRow: Int, inComponent component: Int) {
         selectedPickerValue = pickerData[pickerRow]
         //        handlePickerSelected(selectedRow: pickerRow)
@@ -357,14 +364,12 @@ UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
                 collectionView.dequeueReusableCell(
                     withReuseIdentifier: "SetupProfileBioTitleCell", for: indexPath)
             return cell
-        }
-        if indexPath.row == 1 {
+        } else if indexPath.row == 1 {
             let cell =
                 collectionView.dequeueReusableCell(
                     withReuseIdentifier: "SetupProfileBioInfoCell", for: indexPath)
             return cell
-        }
-        if indexPath.row == 7 {
+        } else if indexPath.row == 7 {
             let cell =
                 collectionView.dequeueReusableCell(
                     withReuseIdentifier: "SetupProfileBioMetric", for: indexPath) as! SetupProfileUnitSelectionCell
@@ -375,43 +380,72 @@ UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
                 cell.unitSwitch.isOn = false
                 cell.unitSwitch.setOn(false, animated: true)
             }
-
+            
+            return cell
+        } else {
+            let cell =
+                collectionView.dequeueReusableCell(
+                    withReuseIdentifier: "SetupProfileBioOptionCell",
+                    for: indexPath) as! SetupProfileBioOptionCell
+            let option = setupProfileOptionList[indexPath.row]
+            cell.logo.image = option?.image
+            cell.label.text = option?.label
+            cell.button.setTitle(option?.buttonText, for: .normal)
+            cell.button.setImage(nil, for: .normal)
+            let isSynced = option?.isSynced
+            if(isSynced == true) {
+                cell.button.layer.borderColor = UIColor.clear.cgColor
+                cell.button.titleLabel?.font = UIFont(name: "Montserrat-SemiBold", size: 18)
+            }else {
+                cell.button.layer.borderColor = #colorLiteral(red: 0.3529411765, green: 0.6549019608, blue: 0.6549019608, alpha: 1)
+                cell.button.titleLabel?.font = UIFont(name: "Montserrat-Medium", size: 14)
+            }
+            cell.delegate = self
+            
+            // SYNC Button
+            if indexPath.row == 2 && cell.label.text == "Sync Apple Health Profile" {
+                if healthKitUtil.isHealthkitSynced {
+                    cell.button.setImage(#imageLiteral(resourceName: "icon: check mark"), for: .normal)
+                    cell.button.tintColor = #colorLiteral(red: 0.4175422788, green: 0.7088702321, blue: 0.7134250998, alpha: 1)
+                    cell.button.setTitle("SYNCED", for: .normal)
+                    cell.button.layer.borderColor = UIColor.clear.cgColor
+                } else{
+                    cell.button.setImage(nil, for: .normal)
+                    cell.button.setTitle("SYNC", for: .normal)
+                    cell.button.layer.borderColor = #colorLiteral(red: 0.3529411765, green: 0.6549019608, blue: 0.6549019608, alpha: 1)
+                }
+            }
+            
+            let verticalLine = UIView()
+            verticalLine.backgroundColor = UIColor(hexString: "#D6D6D6")
+            verticalLine.translatesAutoresizingMaskIntoConstraints = false
+            
+            cell.addSubview(verticalLine)
+            
+            NSLayoutConstraint.activate([
+                verticalLine.widthAnchor.constraint(equalToConstant: 1.5),
+                verticalLine.centerXAnchor.constraint(equalTo: cell.logo.centerXAnchor)
+            ])
+            
+            if indexPath.item == 2 {
+                NSLayoutConstraint.activate([
+                    verticalLine.topAnchor.constraint(equalTo: cell.logo.centerYAnchor),
+                    verticalLine.bottomAnchor.constraint(equalTo: cell.bottomAnchor)
+                ])
+            } else if indexPath.item == 6 {
+                NSLayoutConstraint.activate([
+                    verticalLine.topAnchor.constraint(equalTo: cell.topAnchor),
+                    verticalLine.bottomAnchor.constraint(equalTo: cell.logo.centerYAnchor)
+                ])
+            } else {
+                NSLayoutConstraint.activate([
+                    verticalLine.topAnchor.constraint(equalTo: cell.topAnchor),
+                    verticalLine.bottomAnchor.constraint(equalTo: cell.bottomAnchor)
+                ])
+            }
+            cell.sendSubviewToBack(verticalLine)
             return cell
         }
-
-        let cell =
-            collectionView.dequeueReusableCell(
-                withReuseIdentifier: "SetupProfileBioOptionCell",
-                for: indexPath) as! SetupProfileBioOptionCell
-        let option = setupProfileOptionList[indexPath.row]
-        cell.logo.image = option?.image
-        cell.label.text = option?.label
-        cell.button.setTitle(option?.buttonText, for: .normal)
-        cell.button.setImage(nil, for: .normal)
-        let isSynced = option?.isSynced
-        if(isSynced == true) {
-            cell.button.layer.borderColor = UIColor.clear.cgColor
-            cell.button.titleLabel?.font = UIFont(name: "Montserrat-SemiBold", size: 18)
-        }else {
-            cell.button.layer.borderColor = #colorLiteral(red: 0.3529411765, green: 0.6549019608, blue: 0.6549019608, alpha: 1)
-            cell.button.titleLabel?.font = UIFont(name: "Montserrat-Medium", size: 14)
-        }
-        cell.delegate = self
-
-        // SYNC Button
-        if indexPath.row == 2 && cell.label.text == "Sync Apple Health Profile" {
-            if healthKitUtil.isHealthkitSynced {
-                cell.button.setImage(#imageLiteral(resourceName: "icon: check mark"), for: .normal)
-                cell.button.tintColor = #colorLiteral(red: 0.4175422788, green: 0.7088702321, blue: 0.7134250998, alpha: 1)
-                cell.button.setTitle("SYNCED", for: .normal)
-                cell.button.layer.borderColor = UIColor.clear.cgColor
-            } else{
-                cell.button.setImage(nil, for: .normal)
-                cell.button.setTitle("SYNC", for: .normal)
-                cell.button.layer.borderColor = #colorLiteral(red: 0.3529411765, green: 0.6549019608, blue: 0.6549019608, alpha: 1)
-            }
-        }
-        return cell
     }
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
--- Longevity/Controller/SetupProfile/SetupProfileDevicesVC.swift
+++ Longevity/Controller/SetupProfile/SetupProfileDevicesVC.swift
@@ -121,15 +121,15 @@ extension SetupProfileDevicesVC: UICollectionViewDelegate, UICollectionViewDataS
             cell.layer.shadowOpacity = 0.14
             cell.layer.masksToBounds = false
             if option?.isConnected == true {
-                cell.connectBtn.setTitle("SYNCED", for: .normal)
+                cell.connectBtn.setTitle("ADDED", for: .normal)
                 cell.connectBtn.setTitleColor(#colorLiteral(red: 0.3529411765, green: 0.6549019608, blue: 0.6549019608, alpha: 1), for: .normal)
-                cell.connectBtn.setImage(#imageLiteral(resourceName: "icon: check mark"), for: .normal)
+                cell.connectBtn.setImage(#imageLiteral(resourceName: "icon: checked"), for: .normal)
             } else {
-                let image = UIImage(named: "")
                 cell.connectBtn.setTitle(nil, for: .normal)
                 cell.connectBtn.setImage(#imageLiteral(resourceName: "icon: add"), for: .normal)
             }
-
+            
+            cell.connectBtn.alignVertical(spacing: 5.0)
             cell.delegate = self
             return cell
         }
@@ -147,4 +147,36 @@ extension SetupProfileDevicesVC: UICollectionViewDelegate, UICollectionViewDataS
             return CGSize(width: width - 40, height: CGFloat(80))
         }
     }
+}
+
+extension UIButton {
+  func alignVertical(spacing: CGFloat = 6.0) {
+    guard let imageSize = imageView?.image?.size,
+      let text = titleLabel?.text,
+      let font = titleLabel?.font
+    else { return }
+
+    titleEdgeInsets = UIEdgeInsets(
+      top: 0.0,
+      left: -imageSize.width,
+      bottom: -(imageSize.height + spacing),
+      right: 0.0
+    )
+
+    let titleSize = text.size(withAttributes: [.font: font])
+    imageEdgeInsets = UIEdgeInsets(
+      top: -(titleSize.height + spacing),
+      left: 0.0,
+      bottom: 0.0,
+      right: -titleSize.width
+    )
+
+    let edgeOffset = abs(titleSize.height - imageSize.height) / 2.0
+    contentEdgeInsets = UIEdgeInsets(
+      top: edgeOffset,
+      left: 0.0,
+      bottom: edgeOffset,
+      right: 0.0
+    )
+  }
 }
