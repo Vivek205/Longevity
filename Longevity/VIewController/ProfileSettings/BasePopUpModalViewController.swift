379b93c548955f9a3fc86b4c407cb9fb7a41ca40
Jagan Kumar <mudila.jagan@shrenitexpert.online>
2020-10-14 00:13:13 +0400

    Updated modal dismissal with PanGesture

--- Longevity/VIewController/ProfileSettings/BasePopUpModalViewController.swift
+++ Longevity/VIewController/ProfileSettings/BasePopUpModalViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class BasePopUpModalViewController: UIViewController {
+    
+    private enum ModalDismissDirection {
+        case upwards
+        case downwards
+    }
 
     var showBackdrop: Bool = false {
         didSet {
 //            self.backdrop.isHidden = !self.showBackdrop
         }
     }
+    
+    private var dismissalDirection: ModalDismissDirection = .downwards
     
     lazy var closeButton: UIButton = {
         let close = UIButton()
@@ -70,7 +77,6 @@ class BasePopUpModalViewController: UIViewController {
         self.containerView.addSubview(titleLabel)
         self.containerView.addSubview(infoLabel)
         
-        
         NSLayoutConstraint.activate([
             containerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20.0),
             containerView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20.0),
@@ -94,10 +100,42 @@ class BasePopUpModalViewController: UIViewController {
         tapgesture.numberOfTouchesRequired = 1
         self.view.addGestureRecognizer(tapgesture)
         containerView.center = CGPoint(x: self.view.center.x, y: self.view.center.y * 2)
+        
+        let gesture = UIPanGestureRecognizer(target: self, action: #selector(wasDragged))
+        containerView.addGestureRecognizer(gesture)
     }
     
     @objc func closeView() {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    var viewTranslation = CGPoint(x: 0, y: 0)
+    
+    @objc func wasDragged(sender: UIPanGestureRecognizer) {
+        switch sender.state {
+            case .changed:
+                viewTranslation = sender.translation(in: view)
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                    self.containerView.transform = CGAffineTransform(translationX: 0, y: self.viewTranslation.y)
+                })
+            case .ended:
+                if viewTranslation.y > -100 && viewTranslation.y < 200 {
+                    UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                        self.containerView.transform = .identity
+                    })
+                } else {
+                    
+                    if viewTranslation.y < 0 {
+                        self.dismissalDirection = .upwards
+                    } else {
+                        self.dismissalDirection = .downwards
+                    }
+                    
+                    dismiss(animated: true, completion: nil)
+                }
+            default:
+                break
+            }
     }
     
     override func viewDidLayoutSubviews() {
@@ -128,7 +166,11 @@ class BasePopUpModalViewController: UIViewController {
         
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut) {
             self.view.backgroundColor = UIColor.black.withAlphaComponent(0.50)
-            self.containerView.center = CGPoint(x: self.view.center.x, y: self.view.center.y * 2)
+            if self.dismissalDirection == .downwards {
+                self.containerView.center = CGPoint(x: self.view.center.x, y: self.view.center.y * 2)
+            } else {
+                self.containerView.center = CGPoint(x: self.view.center.x, y: -(self.view.center.y * 2))
+            }
         }
     }
 }
