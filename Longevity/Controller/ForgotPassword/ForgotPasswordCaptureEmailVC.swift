eabe0a5dc6794ccdb8c1ecb419b54c7f10caa507
Vivek205 <ichbinvivek@gmail.com>
2020-10-14 19:39:36 +0530

    Forgot password controllers

---
+++ Longevity/Controller/ForgotPassword/ForgotPasswordCaptureEmailVC.swift
@@ -0,0 +1,30 @@
+//
+//  ForgotPasswordCaptureEmailVC.swift
+//  Longevity
+//
+//  Created by vivek on 14/10/20.
+//  Copyright © 2020 vivek. All rights reserved.
+//
+
+import UIKit
+
+class ForgotPasswordCaptureEmailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
