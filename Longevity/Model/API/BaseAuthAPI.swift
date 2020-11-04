f2868f98f797a75eff10ad72c75912544213b123
Jagan Kumar <mudila.jagan@shrenitexpert.online>
2020-11-04 08:06:40 +0400

    Updated the Amplify Authorisation.

--- Longevity/Model/API/BaseAuthAPI.swift
+++ Longevity/Model/API/BaseAuthAPI.swift
@@ -16,7 +16,42 @@ enum Logintype: String {
     case clinicaltrail = "CLINICAL_TRIAL"
 }
 
+enum APICallType {
+    case apiGET
+    case apiPOST
+    case apiPUT
+    case apiDELETE
+}
+
 class BaseAuthAPI {
+    
+    var headers: [String: String] {
+        return ["token": self.getUserToken() ?? "", "content-type":"application/json", "login_type":LoginType.PERSONAL]
+    }
+    
+    fileprivate func getUserToken() -> String? {
+        let token = try? KeyChain(service: KeychainConfiguration.serviceName, account: KeychainKeys.idToken).readItem()
+        print("Id token - \(token) ")
+        return token
+    }
+    
+    fileprivate func fetchUserToken(isSuccess: @escaping(Bool) -> Void) {
+        _ = Amplify.Auth.fetchAuthSession { result in
+            guard let session = try? result.get() as? AuthCognitoTokensProvider,
+                  let tokens = try? session.getCognitoTokens().get() else {
+                isSuccess(false)
+                return
+            }
+            
+            do{
+                print("Id token - \(tokens.idToken) ")
+                try KeyChain(service: KeychainConfiguration.serviceName, account: KeychainKeys.idToken).saveItem(tokens.idToken)
+                isSuccess(true)
+            } catch {
+                isSuccess(false)
+            }
+        }
+    }
     
     func getCredentials(completion: @escaping (_ credentials: Credentials)-> Void,
                         onFailure: @escaping (_ error: Error)-> Void) {
@@ -87,7 +122,56 @@ class BaseAuthAPI {
             }
         }
     }
-
+    
+    
+    
+    func makeAPICall(callType: APICallType, request: RESTRequest, completion: @escaping(Data?, Error?) -> Void) {
+        if callType == .apiGET {
+            self.getRequest(request: request, completion: completion)
+        } else if callType == .apiPOST {
+            self.postRequest(request: request, completion: completion)
+        }
+    }
+    
+    fileprivate func getRequest(request: RESTRequest, isRetry: Bool = false, completion: @escaping(Data?, Error?) -> Void) {
+        let newRequest = RESTRequest(apiName: request.apiName, path: request.path, headers: self.headers, queryParameters: request.queryParameters, body: request.body)
+        _ = Amplify.API.get(request: request, listener: { (result) in
+            switch result{
+            case .success(let data):
+                completion(data, nil)
+            case .failure(let apiError):
+                if !isRetry {
+                self.fetchUserToken { (success) in
+                    if success {
+                        self.getRequest(request: request, isRetry: true, completion: completion)
+                    }
+                }
+                } else {
+                    completion(nil, apiError)
+                }
+            }
+        })
+    }
+    
+    fileprivate func postRequest(request: RESTRequest, isRetry: Bool = false, completion: @escaping(Data?, Error?) -> Void) {
+        let newRequest = RESTRequest(apiName: request.apiName, path: request.path, headers: self.headers, queryParameters: request.queryParameters, body: request.body)
+        _ = Amplify.API.post(request: request, listener: { (result) in
+            switch result{
+            case .success(let data):
+                completion(data, nil)
+            case .failure(let apiError):
+                if !isRetry {
+                self.fetchUserToken { (success) in
+                    if success {
+                        self.postRequest(request: request, isRetry: true, completion: completion)
+                    }
+                }
+                } else {
+                    completion(nil, apiError)
+                }
+            }
+        })
+    }
 }
 
 extension Decodable {
