1c4d00d294b8dc158a0559c72c1e2f8683a39363
Vivek205 <ichbinvivek@gmail.com>
2021-01-27 12:14:35 +0530

    fixed footerView in covid checkin survey

--- Longevity/Controller/ResearchKitStepVC/FormStepVC.swift
+++ Longevity/Controller/ResearchKitStepVC/FormStepVC.swift
@@ -28,7 +28,7 @@ class FormStepVC: BaseStepViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.continueButton.isEnabled = true
         self.navigationController?.navigationBar.barTintColor = .white
         self.rollbackYOrigin = self.view.frame.origin.y
         
@@ -70,8 +70,7 @@ class FormStepVC: BaseStepViewController {
             formItemsCollection.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
             formItemsCollection.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
             formItemsCollection.topAnchor.constraint(equalTo: self.view.topAnchor),
-            formItemsCollection.bottomAnchor.constraint(equalTo: self.view.bottomAnchor,
-                                                        constant: -footerViewHeight)
+            formItemsCollection.bottomAnchor.constraint(equalTo: footerView.topAnchor)
         ])
 
         guard let layout = formItemsCollection.collectionViewLayout as? UICollectionViewFlowLayout else {
